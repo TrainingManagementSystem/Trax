1f57ad357f671c99290680ab20b27b10bcf3b29c
colinlohner <colin@colinlohner.com>
2016-04-19 11:42:07 -0500

    change trainer

--- ServerAssets/trainer/Trainer.js
+++ ServerAssets/trainer/Trainer.js
@@ -1,15 +1,12 @@
 var mongoose = require('mongoose');
-var deepPopulate = require('mongoose-deep-populate')(mongoose);
 
 var Trainer = new mongoose.Schema({
   firstName: {type:String, required: true},
   lastName: {type:String, required: true},
   email: {type:String, required: true, unique: true, index: true},
   password: {type: String, required:true},
-  stuCode: {type: String, required:true, index:true},
+  phone: {type: String, required:true, index:true},
   trainees: [{type: mongoose.Schema.Types.ObjectId, ref: 'Trainee'}]
 });
-
-Trainer.plugin(deepPopulate);
 
 module.exports = Trainer;
