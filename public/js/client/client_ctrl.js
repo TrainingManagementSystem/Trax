165f824dc56f51ef63565bae3e3ac51586da907d
colinlohner <colin@colinlohner.com>
2016-04-22 16:45:04 -0500

    goal save button

--- public/js/client/client_ctrl.js
+++ public/js/client/client_ctrl.js
@@ -16,12 +16,30 @@ app.controller('client_ctrl', function($scope){
   }
 
   $scope.editingCalGoal = false;
+  $scope.calEditText = 'EDIT';
+  $scope.calEditIcon = 'settings';
   $scope.editCalGoal = function(){
     $scope.editingCalGoal = !$scope.editingCalGoal;
+    if($scope.calEditText === 'SAVE'){
+      $scope.calEditText = 'EDIT';
+      $scope.calEditIcon = 'settings';
+    }else{
+      $scope.calEditText = 'SAVE';
+      $scope.calEditIcon = 'save';
+    }
   }
 
   $scope.editingStepGoal = false;
+  $scope.stepEditText = 'EDIT';
+  $scope.stepEditIcon = 'settings';
   $scope.editStepGoal = function(){
     $scope.editingStepGoal = !$scope.editingStepGoal;
+    if($scope.stepEditText === 'SAVE'){
+      $scope.stepEditText = 'EDIT';
+      $scope.stepEditIcon = 'settings';
+    }else{
+      $scope.stepEditText = 'SAVE';
+      $scope.stepEditIcon = 'save';
+    }
   }
 });
